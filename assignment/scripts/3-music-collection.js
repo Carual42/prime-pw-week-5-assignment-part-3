384d9fe0b8801d7de035bd75398dd8c4226c0f14
Alex Carufel <Carual42@gmail.com>
2022-07-01 21:21:21 -0500

    get rickrolled

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,9 +11,16 @@ function addToCollection(title, artist, yearPublished){
         'year published': yearPublished
     }
     collection.push(newSong)
-    return collection
+    return newSong
 }
 // console.log(addToCollection('made up', 'alex', 2022))
 // collection.push(addToCollection('made up', 'alex', 2022))
-addToCollection('made up', 'mario', 2022)
+addToCollection('never', 'gonna', 1987)
+addToCollection('give', 'you', 1987)
+addToCollection('up', 'never,', 1987)
+addToCollection('gonna', 'let', 1987)
+addToCollection('you', 'down', 1987)
+addToCollection('never', 'gonna', 1987)
+addToCollection('run', 'around and', 1987)
+addToCollection('desert', 'you', 1987)
 console.log(collection)
