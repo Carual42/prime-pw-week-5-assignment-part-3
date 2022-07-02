86e5fc4e099c9a11ee256a1e892cc2eb70431747
Alex Carufel <Carual42@gmail.com>
2022-07-01 21:11:44 -0500

    add to collection function done

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,19 @@
 console.log('***** Music Collection *****')
+
+// collection (empty) array
+let collection = [];
+
+// function to add a new object (with param title, artist, yearPublished) to collection
+function addToCollection(title, artist, yearPublished){
+    let newSong = {
+        'title': title,
+        'artist': artist,
+        'year published': yearPublished
+    }
+    collection.push(newSong)
+    return collection
+}
+// console.log(addToCollection('made up', 'alex', 2022))
+// collection.push(addToCollection('made up', 'alex', 2022))
+addToCollection('made up', 'mario', 2022)
+console.log(collection)
