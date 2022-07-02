10d5af630d0e2f42e49e23581db499b1c6f3d118
Alex Carufel <Carual42@gmail.com>
2022-07-01 21:38:23 -0500

    show collection function done

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -6,21 +6,34 @@ let collection = [];
 // function to add a new object (with param title, artist, yearPublished) to collection
 function addToCollection(title, artist, yearPublished){
     let newSong = {
-        'title': title,
-        'artist': artist,
-        'year published': yearPublished
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
     }
     collection.push(newSong)
     return newSong
 }
 // console.log(addToCollection('made up', 'alex', 2022))
 // collection.push(addToCollection('made up', 'alex', 2022))
+
+// add to collection
 addToCollection('never', 'gonna', 1987)
 addToCollection('give', 'you', 1987)
-addToCollection('up', 'never,', 1987)
+addToCollection('up', 'never', 1987)
 addToCollection('gonna', 'let', 1987)
 addToCollection('you', 'down', 1987)
 addToCollection('never', 'gonna', 1987)
 addToCollection('run', 'around and', 1987)
 addToCollection('desert', 'you', 1987)
-console.log(collection)
+
+// show collection
+console.table(collection)
+
+// function to show collection
+function showCollection(array){
+    console.log('The collection is', array.length, 'long');
+    for (let i = 0; i<array.length; i++){
+        console.log(array[i].title, 'by', array[i].artist + ', published in', array[i].yearPublished)
+    }
+}
+console.table(showCollection(collection))
